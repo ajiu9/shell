eb84a04b2d77b6ca206b54aca4e9eb50e93421e3
ajiu9 <615944323@qq.com>
2023-09-22 14:44:31 +0800

    chore: add obsidian daily template node script

--- packages/daily/index.js
+++ packages/daily/index.js
@@ -4,7 +4,7 @@ import fs from 'node:fs/promises'
 import { createRequire } from 'node:module'
 import minimist from 'minimist'
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+// const __dirname = fileURLToPath(new URL('.', import.meta.url))
 const require = createRequire(import.meta.url)
 const args = minimist(process.argv.slice(2))
 const targets = args._
@@ -27,19 +27,23 @@ async function run() {
 
 function getCurrentTime(now) {
   const year = now.getFullYear()
-  const month = now.getMonth() + 1
-  const day = now.getDate()
+  const month = `${now.getMonth() + 1}`
+  const day = `${now.getDate()}`
+  const week = `${getWeek()}`
   return {
     year,
     month,
     day,
-    time: `${year}-${month}-${day}`,
-    week: `${year}-W-${getWeek()}`,
+    time: `${year}-${month.length === 1 ? padLefZero(month) : month}-${day.length === 1 ? padLefZero(day) : day}`,
+    week: `${year}-W-${week.length === 1 ? padLefZero(week) : week}`,
   }
   function getWeek() {
     const firstDayOfYear = new Date(year, 0, 1)
     const firstWeekStartDay = firstDayOfYear.getDay()
     const diffDays = Math.ceil((now - (firstDayOfYear - firstWeekStartDay * 86400000)) / 86400000)
     return Math.ceil(diffDays / 7)
+  }
+  function padLefZero(str) {
+    return (`00${str}`).substr(str.length)
   }
 }
