225feba4a517a5dc3216d04d47276fedb5425af6
ajiu9 <615944323@qq.com>
2024-03-25 09:48:38 +0800

    chore: fix cmd weekly

--- packages/obsidian/index.js
+++ packages/obsidian/index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-import { fileURLToPath } from 'node:url'
 import fs from 'node:fs/promises'
 import { createRequire } from 'node:module'
 import minimist from 'minimist'
 import { formatDate, getTasksData } from './utils/index.js'
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+// const __dirname = fileURLToPath(new URL('.', import.meta.url))
 const require = createRequire(import.meta.url)
 const args = minimist(process.argv.slice(2))
 const targets = args._
@@ -18,13 +17,13 @@ run()
 async function run() {
   const now = new Date()
   if (args.next && ['daily', 'saturday', 'sunday'].includes(target)) now.setDate(now.getDate() + 1)
-  if (args.next && target === 'week') now.setDate(now.getDate() + 7)
+  if (args.next && target === 'weekly') now.setDate(now.getDate() + 7)
   const currentTime = formatDate(now)
   const nameEnum = {
     daily: 'time',
     saturday: 'time',
     sunday: 'time',
-    week: 'week',
+    weekly: 'week',
     empty: 'empty',
     task: 'task',
   }
@@ -45,7 +44,7 @@ async function run() {
   return fs.writeFile(`${config[target].target}/${fileName}.md`, targetTemplateData)
 
   function getTargetTemplateData(data) {
-    if (target === 'week') {
+    if (target === 'weekly') {
       const time = now
       time.setDate(time.getDate() + 7)
       const { week } = formatDate(time)
